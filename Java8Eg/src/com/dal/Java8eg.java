9822e62994c0b7fd6e561270b5cbb8f316efd1f7
KavyaV03 <130138895+KavyaV03@users.noreply.github.com>
2023-04-10 12:15:13 +0530

    Add files via upload

--- Java8Eg/src/com/dal/Java8eg.java
+++ Java8Eg/src/com/dal/Java8eg.java
@@ -1,14 +1,13 @@
 package com.dal;
 
 interface Additionn {
-	public int add(int a, int b);.
+	public int add(int a, int b);
 	public int sum(int a, int b);
 }
 
 public class Java8eg {
 
 	public static void main(String[] args) {
-		//	Func_inter var=()->{}
 		Additionn  sum=(a,b)->{
 			return a+b;
 		};
